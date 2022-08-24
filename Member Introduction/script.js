569dac2e0dfef7e39ebb591a155bbf7fe60679d6
Phutanate Pisutsin <pearlphutanate@gmail.com>
2022-08-24 17:06:28 +0800

    Initialize #1

---
+++ Member Introduction/script.js
@@ -0,0 +1,300 @@
+memberInfo = {
+    1:{
+        "fullname":"Justin Keane Hanov",
+        "nationality": "Indonesian 🇮🇩",
+        "description": "Finally we have Keane, the President of ISA!! He is always energetic and full of energy which lights up the mood in whatever room he steps into! ⚡ He's funny and likes to laugh a lot, which makes him easy to get along with. 😆 During his free time, he loves watching movies, hanging out with friends, and playing badminton! Anyways, if you see Keane around, don't be afraid to say hi, trust me, you won't be disappointed! 😉",
+        "study": "IBBA / 1",
+        "position": "President", 
+        "photolink": "https://images.squarespace-cdn.com/content/v1/51cd143fe4b071b5e7edb471/1648735060979-8LJ7WM4FK98IHNFALTTU/IMG_3500.JPG",
+        "positionphoto": "https://images.squarespace-cdn.com/content/v1/51cd143fe4b071b5e7edb471/1648734357600-W0VKUKC9GDU6PA060X50/12.+President.png"
+    },
+    2:{
+        "fullname": "Betty Sek",
+        "nationality": "Hong Kong 🇭🇰",
+        "description": "This is Betty, our beloved VP! She studies the English language, but she wants to pursue a career in the cosmetic industry. A little about her, she loves Korean food, or any kind as long as it's not spicy. She also loves to spend her time studying alone.",
+        "study": "English / 2",
+        "position": "Vice President",
+        "photolink": "https://images.squarespace-cdn.com/content/v1/51cd143fe4b071b5e7edb471/1648735065394-32QI1M3I3TPPWSLBPVB6/7b3f126f-6137-4038-be27-ee352e136f78.jpg",
+        "positionphoto": "https://images.squarespace-cdn.com/content/v1/51cd143fe4b071b5e7edb471/1648734356880-1L9S1DIDMSCFIE5CYH1Z/11.+Vice+President.png"
+    },
+    3:{
+        "fullname": "Juan Fraderick",
+        "nationality": "Indonesian 🇮🇩",
+        "description": "Our secretary right here loves to hike. Introducing Cedrick, the calm and cool guy 😉 (he hates noisy people like me lmao). You can also call him “Cang”. He has a unique clothing style and is always giving out dad jokes. His favorite genre is bedroom pop indie. Guess what? He listens to Clairo and girl in red.",
+        "study": "GLEF / 1",
+        "position": "Secretary",
+        "photolink": "https://images.squarespace-cdn.com/content/v1/51cd143fe4b071b5e7edb471/1648735048057-R9B76P01SX2JC08LCN6X/IMG_3475.JPG",
+        "positionphoto": "https://images.squarespace-cdn.com/content/v1/51cd143fe4b071b5e7edb471/1648734342289-LCOH0OV0CTJ0W7SSD86R/10.+Secretary.png"
+    },
+    4:{
+        "fullname": "Raisa Veronica Adjie",
+        "nationality": "Indonesian 🇮🇩",
+        "description": "Next up, is our sole Event Manager- Raisa from Indonesia. If you are in our ISA discord, you will learn soon how much she is into gaming. She is very easy to approach, especially when you invite her for grabbing sushi (her favorite food). When she's not climbing uphill to Lee Woo Sing or going out for Wanchai Wednesdays (the first place I met her), she loves to bake! She is also amazingly smart. So much so that we are not worried that we do not have two event managers this time. Because Raisa is there to take it all on!",
+        "study": "QFIN / 1",
+        "position": "Event Manager",
+        "photolink": "https://images.squarespace-cdn.com/content/v1/51cd143fe4b071b5e7edb471/1648735020618-GT88LGMD5GDZY6JZNG88/IMG_3460.JPG",
+        "positionphoto": "https://images.squarespace-cdn.com/content/v1/51cd143fe4b071b5e7edb471/1648734330251-8RFVTSYAOAWACDC6Y2TE/7.+Event+Manager.png"
+    },
+    5:{
+        "fullname": "Atsushi Imoto",
+        "nationality": "Hong Kong 🇭🇰 Japanese 🇯🇵",
+        "description": "Next up we have Sushi, our ISA Treasurer!!! Not only is he committed to ISA, he has 6 other ongoing societies 🤩 who else is impressed with that! Aside from committee work, he enjoys gaming 🎮, watching kdramas 📺, Formula 1 🏎, and clubbing.",
+        "study": "ECON / 1",
+        "position": "Treasurer",
+        "photolink": "https://images.squarespace-cdn.com/content/v1/51cd143fe4b071b5e7edb471/1648735080940-8QH0Q8QXWLIP4Z4OI006/IMG_3533.JPG",
+        "positionphoto": "https://images.squarespace-cdn.com/content/v1/51cd143fe4b071b5e7edb471/1648734340201-JJ5PUK2GM4Z123SSTJYL/9.+Treasurer.png"
+    },
+    6:{
+        "fullname": "Phutanate Pisutsin",
+        "nationality": "Thai 🇹🇭",
+        "description": "Introducing our IT Officer: Pearl, from Thailand, is a proud Morningsider. He is one of the funniest ExCo in this year's cabinet, or at least we think he is🤣. But don’t underestimate him since he is one of the most creative and hardworking people that we know! His commitment to his work as an ExCo inspires us to do better! He loves Thai food, basketball, and reading! Pearl is fun to talk to and outgoing! So just dm him if you want to hangout😉!",
+        "study": "ELEG / 1",
+        "position": "IT Officer",
+        "photolink": "https://images.squarespace-cdn.com/content/v1/51cd143fe4b071b5e7edb471/1648734990389-666Z04OT54XDAYH2YIOA/IMG_3426.JPG",
+        "positionphoto": "https://images.squarespace-cdn.com/content/v1/51cd143fe4b071b5e7edb471/1648734310122-39WNK2DN0OIHWH1E2S41/4.+IT+Officer.png"
+    },
+    7:{
+        "fullname": "Anujit Saha",
+        "nationality": "Bangladeshi 🇧🇩",
+        "description": "Now, let us introduce our savvy PR officer-Anujit from Bangladesh !!🎉 You can be sure to find him when you are looking for a hiking mate, decent chef, or even someone to have a lil' chat with about politics. If you don't see him chillaxing at his fav spot 😎, the rooftop of WYS, you may assume he is busy thinking of captions for our ISA post (literally) or responding to your DMs 🤣.",
+        "study": "BMEG / 1",
+        "position": "PR Officer",
+        "photolink": "https://images.squarespace-cdn.com/content/v1/51cd143fe4b071b5e7edb471/1648735010904-ALN6DU7NDJGB2X4FURSI/IMG_3439.JPG",
+        "positionphoto": "https://images.squarespace-cdn.com/content/v1/51cd143fe4b071b5e7edb471/1648734327856-WQQ3OUKQHF30XJSBC3IE/6.+PR+Manager.png"
+    },
+    8:{
+        "fullname": "Enyu Liu",
+        "nationality": "Taiwanese 🇹🇼",
+        "description": "Onto our Design Officer, we have here the-canva-master Enyu from Taiwan!! Enyu enjoys watching movies and series online to chill. She just finished Emily in Paris 🤳🏽 and waiting for Bridgerton S2 to come out 👑 any Bridgerton fans out thereeee 👀 Enyu also likes playing board games and sometimes bake and play badminton!!",
+        "study": "IBBA / 2",
+        "position": "Design Officer",
+        "photolink": "https://images.squarespace-cdn.com/content/v1/51cd143fe4b071b5e7edb471/1648734977194-O70M2ISBPAH0MYXNUMGG/ExCo+Member+Introduction.png",
+        "positionphoto": "https://images.squarespace-cdn.com/content/v1/51cd143fe4b071b5e7edb471/1648734310970-A8C38J66ZMGFJR2LR2LT/5.+Design+Officer.png"
+    }
+}
+
+
+
+let index = 1;
+let description = document.getElementById("desc-text");
+let personalinfo = document.getElementById("personalinfo");
+let namee = document.getElementById("fullname");
+let title = document.getElementById("title");
+let img = document.getElementById("image");
+
+show();
+
+function show(){
+    let blursquareReset = document.getElementsByClassName("inphotobar");
+    for(let i =0; i<blursquareReset.length; i++)
+        blursquareReset[i].style.opacity = 0.25;
+    namee.innerHTML = memberInfo[index].fullname;
+    personalinfo.innerHTML = memberInfo[index].nationality + " | " + memberInfo[index].study;
+    description.innerHTML = memberInfo[index].description;
+    title.innerHTML = memberInfo[index].position;
+    img.setAttribute("src", memberInfo[index].photolink);
+    document.getElementById("prevbut").style.display = "block";
+    document.getElementById("nextbut").style.display = "block";
+    if(index==1)
+        document.getElementById("prevbut").style.display = "none";
+
+    if(index==8)
+        document.getElementById("nextbut").style.display = "none";
+
+    if(index == 6)
+        document.getElementById("description").style.fontSize = '12px';
+    else if(index == 4)
+        document.getElementById("description").style.fontSize = '11px';
+    else document.getElementById("description").style.fontSize = '12.5px';
+    let a = document.getElementById("image").style;
+    a.objectPosition = "54% 50%";
+    if(index == 5) a.objectPosition = "47% 50%";
+    if(index == 6) a.objectPosition = "60% 50%";
+    if(index == 7) a.objectPosition = "52% 50%";
+    let blurid = "pic" + index;
+    let blursquare = document.getElementById(blurid);
+    blursquare.style.opacity = 1;
+}
+
+function shownext(){
+    if(index<8)
+        index = index + 1;
+    show();
+}
+
+function showprev(){
+    if(index>1)
+        index = index-1;
+    show();
+}
+
+document.getElementById("nextbut").onclick = function() {shownext()};
+document.getElementById("prevbut").onclick = function() {showprev()};
+
+let ball = document.getElementsByClassName("circle");
+for(let i = 0; i < ball.length; i++){
+    ball[i].onmousemove = function(){collision(i, event)};
+}
+// function findTouchedBall(event){
+//     console.log("x = ", event.clientX, " Y = ", event.clientY);
+// }
+
+function findTouchedDrction(ballposX, ballposY, mouseposX, mouseposY){
+    if(mouseposX<ballposX+25 && mouseposX>=ballposX && mouseposY<=ballposY && mouseposY>ballposY-25)
+        return 1;
+    if(mouseposX<ballposX+25 && mouseposX>=ballposX && mouseposY>ballposY && mouseposY<ballposY+25)
+        return 2;
+    if(mouseposX<ballposX && mouseposX>ballposX-25 && mouseposY>ballposY && mouseposY<ballposY+25)
+        return 3;
+    if(mouseposX<ballposX && mouseposX>ballposX-25 && mouseposY<=ballposY && mouseposY>ballposY-25)
+        return 4;
+    return -1;
+}
+
+function moveit(i, direction){
+    //ใว้มาทำต่อ  ดูvideoของbeau fcc
+    let selectedball = ball[i];
+    let pos = 0;
+    let id = setInterval(frame, 30);
+    function frame(){
+        if(pos == 150)
+            clearInterval(id);
+        else{
+            pos += 3;
+            ballposX = window.getComputedStyle(selectedball, null).getPropertyValue("left");
+            ballposY = window.getComputedStyle(selectedball, null).getPropertyValue("top");
+            ballposX = Number(ballposX.slice(0, -2));
+            ballposY = Number(ballposY.slice(0, -2));
+            let additionY = 0.27, additionX = 0.27;
+            if(direction==1)
+                additionX*=-1;
+            else if(direction==2){
+                additionY*=-1;
+                additionX*=-1;
+            }
+            else if(direction==3)
+                additionY*=-1;
+            /*if(ballposX<=20 || ballposX>=900 || ballposY<=20 || ballposY>=650){
+                skip = true;
+            }*/
+            ballposX += additionX;
+            ballposY += additionY;
+            selectedball.style.top = ballposY + 'px';
+            selectedball.style.left = ballposX + 'px';
+        }
+    }
+}
+let stop = false;
+let outchecker = [];
+function collision (i, event){
+        //if(!stop/* && !(i in outchecker)*/){
+        ballposX = window.getComputedStyle(ball[i], null).getPropertyValue("left");
+        ballposY = window.getComputedStyle(ball[i], null).getPropertyValue("top");
+        ballposX = Number(ballposX.slice(0, -2)) + 25 +40*i;
+        ballposY = Number(ballposY.slice(0, -2)) + 25;
+        let mouseposX = event.pageX;
+        let mouseposY = event.pageY;
+        console.log("ball number ", i, " mouse pos ", mouseposX, mouseposY);
+        console.log("ball number ", i, " ball pos ", ballposX, ballposY);
+        let direction = findTouchedDrction(ballposX, ballposY, mouseposX, mouseposY);
+        //if(direction == -1)
+            //return;
+        //console.log("direction is ", direction);
+        moveit(i, direction);
+        //}
+        if(ballposX<=20 || ballposX>=950 || ballposY<=20 || ballposY>=600){
+            //stop=true;
+            ball[i].style.opacity = "0";
+            //outchecker.push(i);
+        }
+        //stop=false;
+}
+
+function photobarshow(i){
+    index = i;
+    show();
+}
+
+let imagesinbar = document.getElementsByClassName("inbarimage");
+for(let i=0; i<imagesinbar.length; i++){
+    var attr = document.createAttribute("src");
+    //attr.value="";
+    imagesinbar[i].setAttributeNode(attr);
+    imagesinbar[i].setAttribute("src", memberInfo[i+1].positionphoto);
+}
+let photobar = document.getElementsByClassName("inphotobar");
+for(let i=0; i<photobar.length; i++){
+    photobar[i].onclick = function(){photobarshow(i+1)};
+}
+
+/*function repel(){
+    let posX, posY;
+    window.addEventListener('mousemove', function(e){
+        let which = findTouchedBall(e.x, e.y);
+    })
+}
+
+
+for(let i of ball){
+    i.onmousemove = function(){
+        repel();
+    };
+} 555*/
+
+
+//document.getElementById("yo").online = function() {show()};
+
+/*function getCoor(){
+    let posX, posY;
+    window.addEventListener('mousemove', function(e){
+        posX = e.x;
+        posY = e.y;
+    });
+    console.log("pos X = ", posX);
+    return [posX, posY];
+}*/
+
+/*let checkk;
+
+function repel(){
+    if(checkk){
+        let posX, posY;
+        window.addEventListener('mousemove', function(e){
+            console.log(e.x);
+            //posX = e.x;
+            //posY = e.y;
+        });
+    }
+    else{ 
+        window.removeEventListener('mousemove', function(e){
+            console.log(e.x);
+        }); 
+    }
+    //console.log(posX, posY);
+}
+
+function stop(){
+    checkk = false;
+    console.log(99);
+    window.removeEventListener('mousemove', function(e){
+        console.log(e.x);
+    });
+}
+
+//repel();
+
+document.getElementById("tt").onmousemove = function() {
+    checkk = true;
+    console.log(55);
+    //repel();
+    window.addEventListener('mousemove', function(e){
+        console.log(e.x);
+        //posX = e.x;
+        //posY = e.y;
+    });
+};
+document.getElementById("tt").onmouseout = function() {
+    window.removeEventListener('mousemove', function(e){
+        console.log(e.x);
+    });
+};*/
